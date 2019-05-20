ea7687a9a18e959cbd8bc3cb6586785b4d59a440
Alexander Riccio <test35965@gmail.com>
2019-05-20 16:16:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
-def reverse_each_word(str)
-  reversed = ""
-  str.split().collect() do |word|
-    reversed += word.reverse()
+def reverse_each_work(str)
+  split_str = str.split()
+  split_str.collect() do |word|
+    word = word.reverse()
   end
-  
 end
